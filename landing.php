dd51ed9e9a201eb7393ff7968d80f8a741f6788f
Alina Puro <44803321+alinapww@users.noreply.github.com>
2018-11-06 12:47:52 +0200

    Add files via upload

---
+++ landing.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+ <meta charset="utf-8">
+ <meta http-equiv="X-UA-Compatible" content="IE=edge">
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+ <title>Rent A Tuxedo</title>
+ <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdjns.cloudfare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style type="text/css">
+.jumbotron {
+  background: url("Pictures/tuxedo.png");
+  width: 100%;
+  height: 400px;
+  background-size: cover;
+  color: black;
+  padding-right: 230px;
+}
+body{
+background:white;
+background-size:cover;
+}
+</style>
+</head>
+<body>
+<nav class="navbar navbar-inverse">
+<div class="container-fluid">
+<div class="navbar-header">
+<a href="landing.php" class="btn btn-default btn-md">
+<span class="glyphicon glyphicon-home" aria-hidden="true"></span>
+Homepage</a>
+</div>
+</div>
+</nav>
+<div class="jumbotron text-right">
+<h1> Rent A Tuxedo </h1>
+<p> The most easiest way to look great on a night out!</p>
+</div>
+</div>
+</div>
+</body>
+</html>
